9a3bc651ad73fc74559bc9174e71e35dbab98d01
maianhkhoi <maianhkhoi04@gmail.com>
2026-06-24 18:06:32 +0700

    validate

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty({
+    message: 'Email is required',
+  })
   email: string;
+
+  @IsNotEmpty()
   password: string;
   name: string;
   address: string;
